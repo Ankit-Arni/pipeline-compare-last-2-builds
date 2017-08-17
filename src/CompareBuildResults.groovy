f414915f9085aa97d4cc12c8e6ad47cd1e9678cb
Ankit-Arni <ankit.arni.1995@gmail.com>
2017-08-17 11:05:43 +0530

    Update CompareBuildResults.groovy

--- src/CompareBuildResults.groovy
+++ src/CompareBuildResults.groovy
@@ -2,10 +2,10 @@ def compareCurrentAndPrevious() {
     if(currentBuild.result == null)
         currentBuild.result = 'SUCCESS'
     def previousBuild = currentBuild.getPreviousBuild()
-    println "Build: ${currentBuild.number} | Result: ${currentBuild.result}"
-    println "Build: ${previousBuild.number} | Result: ${previousBuild.result}"
+    echo "Build: ${currentBuild.number} | Result: ${currentBuild.result}"
+    echo "Build: ${previousBuild.number} | Result: ${previousBuild.result}"
     if (currentBuild.result != previousBuild.result)
-        println "Build result has changed: Previous build = ${previousBuild.result} -> Current build = ${currentBuild.result}"
+        echo "Build result has changed: Previous build = ${previousBuild.result} -> Current build = ${currentBuild.result}"
     else    
-        println "Build result has not changed: Still ${currentBuild.result}"
+        echo "Build result has not changed: Still ${currentBuild.result}"
 }
